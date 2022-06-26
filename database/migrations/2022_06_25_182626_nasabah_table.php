0f1daa7b18c178778df53af3630d43f3d8070a2f
pilar <pilar@student.uns.ac.id>
2022-06-26 19:42:03 +0700

    make migration

---
+++ database/migrations/2022_06_25_182626_nasabah_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nasabah', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('postal_code');
+            $table->string('photo');
+            $table->string('status');
+            $table->string('remember_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
